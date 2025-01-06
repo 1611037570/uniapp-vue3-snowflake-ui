d58399163b054c58342ff387c70b5950665d8bfd
xiaoyang <1611037570@qq.com>
2025-01-06 14:19:13 +0800

    优化脚手架

---
+++ src/components/snowflake-image/config/describe.ts
@@ -0,0 +1,5 @@
+const describe = {
+    name: "图片懒加载",
+    version: "1.0.0",
+    describe: "图片懒加载",
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
+// Vite 配置
 import { defineConfig } from "vite";
+// UniApp 插件
 import uni from "@dcloudio/vite-plugin-uni";
+
+// 自动导入Api
+import AutoImport from "unplugin-auto-import/vite";
+
+// 自动导入组件
+import Components from "@uni-helper/vite-plugin-uni-components";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+    plugins: [
+        // 使用自动导入组件 必须放在uni之前
+        Components({
+            dts: "src/types/components.d.ts",
+        }),
+        // 使用UniApp 插件
+        uni(),
+
+        // 使用自动导入
+        AutoImport({
+            imports: ["vue", "uni-app"],
+            dts: "src/types/auto-imports.d.ts",
+            // dirs: ["src/hooks"], // 自动导入 hooks
+        }),
+    ],
 });
