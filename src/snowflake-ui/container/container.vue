9000a3fef904b40d69348a23d6ad01f6265e07a2
xiaoyang <1611037570@qq.com>
2025-01-21 10:17:16 +0800

    全局控制组件

---
+++ src/snowflake-ui/container/container.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { provide } from "vue";
+const props = defineProps({
+    size: {
+        type: String,
+        default: "small",
+    },
+});
+provide("globalSize", {
+    size: props.size,
+});
+</script>
+
+<template>
+    <view style="width: 100%; height: 100%"><slot></slot> </view>
+</template>
+<style lang="scss" scoped></style>
---
+++ src/snowflake-ui/setItem/setItem.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+    <view style="width: 100%; height: 52px; border-radius: 6px"> </view>
+</template>
+<style lang="scss" scoped></style>
